d05c9632fcb9d41bf03b03cb459d49a6adbc97cb
Andi Gashi <andigashi2005@gmail.com>
2023-07-20 18:00:00 +0200

    Update server code

--- backend.js
+++ backend.js
@@ -1,142 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-let database = {
-    users: [
-        {
-            id: 123,
-            name: 'andi',
-            email: 'andigashi2005@gmail.com',
-            password: 'alienilahet321',
-            entries: 0,
-            joined: new Date()
-
-        },
-        {
-            id: 124,
-            name: 'labi',
-            email: 'labigashi2005@gmail.com',
-            password: 'alienilahet321',
-            entries: 0,
-            joined: new Date()
+const express = require('express')
+const knex = require('knex')
 
 
-        }
-    ],
-    login: [
-        {
-            id: 337,
-            hash: '',
-            email: 'andigashi2005@gmail.com'
-        }
-    ]
-}
-app.get('/', (req, res) => {
-    res.json(database.users);
+
+app.get('/', (res, req) => {
+    db.select('*').from('users').then(response => {
+        res.json(response)
+    })
+
 })
 
-app.post('/signin', (req, res) => {
 
-    bcrypt.compare(apples, hash, function (err, result) {
-        console.log('first guess', res)
-    });
-    bcrypt.compare(cookies, hash, function (err, result) {
-        console.log('second guess', res)
-    });
-
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('success')
-    } else {
-        res.status(400).json('Account invalid')
-    }
-})
 
 
 app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-        console.log(hash)
-    });
-    database.users.push(
-        {
-            id: 125,
-            name: name,
-            email: email,
-            passwordd: password,
-            entries: 0,
-            joined: new Date()
+    const { email, name, password } = req.body
+    db('users').returning('*').insert({
+        email: email,
+        name: name,
+        joined: new Date()
+    }).then(response => {
+        if (response.length) {
+            res.json(response[0])
+        } else {
+            res.status(400).json("You couldn't register")
         }
-
-    )
-    res.json(database.users[database.users.length() - 1])
+    })
+        .catch(err => { res.status(400).json('invalid user') })
 })
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    for (let x of database.users) {
-        if (id === x.id) {
-            found = true
-            res.json(x)
-        }
-
-    }
-    if (!found) {
-        res.json('invalid id')
-    }
+app.get('/profile:id', (req, res) => {
+    const { id } = req.params
+    db.select('*')
+        .from('users').
+        where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('invalid user')
+            }
+        })
+        .catch(err => { res.status(400).json('invalid user') })
 })
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false
-    for (let x of database.users) {
-        if (id = x.id) {
-            found = true
-            x.entries++
-            res.json(x.entries)
-        }
-        if (!found) {
-            res.json('invalid id')
-        }
-    }
+app.put('/entries:id', (req, res) => {
+    const { id } = req.params
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => { res.status(400).json('invalid id') })
 })
 
-// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-//     // Store hash in your password DB.
-// });
+app.post('/sign in',(req,res)={
 
-// // Load hash from your password DB.
-// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
-//     // result == true
-// });
-// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
-//     // result == false
-// });
-
-app.listen(3000, () => {
-    console.log('I am listening bro')
-})
-
-.then(user=>{
-    db2=db.select('*')from('users').then(user=>return user)
-    found=false
-    for(let x of db2){
-        if(x===user){
-            found=TRUE
-            return res.json(x)
-        }
-    }
-    if(!found){
-        res.status(400).json('invalid id')
-    }
-
-})
-update users set entries +=1 where id=id 
-
-db('users').where(id,=,id).update({
-    entries: entrires+1
 })
